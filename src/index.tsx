4dc5a478effa3d89f3fe1972cda50edc4aae3db2
Manuel Trinidad <manuel.trinidad.mo@gmail.com>
2022-06-20 14:15:44 +0300

    👨‍💻 Initial commit!

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import Test from 'components/Test';
+import './index.css';
+
+ReactDOM.render(
+  <StrictMode>
+    <Test />
+  </StrictMode>,
+  document.getElementById('root')
+);
